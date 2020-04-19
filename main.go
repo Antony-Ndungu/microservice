f5cfda33b30749fc62d773296545fd7c622cc356
Antony Ndungu Maina <antony.ndungu.maina@gmail.com>
2020-04-19 20:38:18 +0300

    Reading JSON data in the request

--- main.go
+++ main.go
@@ -3,32 +3,73 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 const (
-	message = "Hello world!"
+	message = "Hello"
 	port    = 8080
 )
+
+type helloWorldRequest struct {
+	Name string `json:"name"`
+}
 
 type helloWorldResponse struct {
 	Message string `json:"message"`
 }
 
+var request helloWorldRequest
 var response = helloWorldResponse{Message: message}
 
 func main() {
+	server()
+}
+
+func server() {
 	http.HandleFunc("/hello-world", helloWorldHandler)
+	http.HandleFunc("/hello-world-unmarshal", helloWorldUnMarshalHandler)
 	log.Printf("Server is starting on port %v\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
 }
 
+func helloWorldUnMarshalHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("{\"error\": %v }", fmt.Sprintf("\"%v\"", http.StatusText(http.StatusBadRequest))), http.StatusBadRequest)
+		return
+	}
+
+	err = json.Unmarshal(body, &request)
+
+	if err != nil {
+		http.Error(w, fmt.Sprintf("{\"error\": %v }", fmt.Sprintf("\"%v\"", http.StatusText(http.StatusBadRequest))), http.StatusBadRequest)
+		return
+	}
+
+	encoder := json.NewEncoder(w)
+	response = helloWorldResponse{Message: fmt.Sprintf("%v %v", message, request.Name)}
+	err = encoder.Encode(&response)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("{\"error\": %v }", fmt.Sprintf("\"%v\"", http.StatusText(http.StatusInternalServerError))), http.StatusInternalServerError)
+	}
+}
+
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	encoder := json.NewEncoder(w)
-	err := encoder.Encode(&response)
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&request)
 	if err != nil {
-		http.Error(w, fmt.Sprintf("{\"error\": %v }", http.StatusText(http.StatusInternalServerError)), http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("{\"error\": %v }", fmt.Sprintf("\"%v\"", http.StatusText(http.StatusBadRequest))), http.StatusBadRequest)
+		return
+	}
+	encoder := json.NewEncoder(w)
+	response = helloWorldResponse{Message: fmt.Sprintf("%v %v", message, request.Name)}
+	err = encoder.Encode(&response)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("{\"error\": %v }", fmt.Sprintf("\"%v\"", http.StatusText(http.StatusInternalServerError))), http.StatusInternalServerError)
 	}
 }
--- main_test.go
+++ main_test.go
@@ -1,12 +1,49 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"testing"
 )
 
 var writer = ioutil.Discard
+
+func init() {
+	go server()
+}
+
+func BenchmarkZero(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		resp, err := http.Post("http://localhost:8080/hello-world", "application/json", bytes.NewBuffer([]byte(`{"name": "Antony"}`)))
+		if err != nil || resp.StatusCode != http.StatusOK {
+			log.Fatal(resp.StatusCode, err)
+		}
+		decoder := json.NewDecoder(resp.Body)
+		err = decoder.Decode(&request)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkZeroUnMarshal(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		resp, err := http.Post("http://localhost:8080/hello-world-unmarshal", "application/json", bytes.NewBuffer([]byte(`{"name": "Antony"}`)))
+		if err != nil || resp.StatusCode != http.StatusOK {
+			log.Fatal(resp.StatusCode, err)
+		}
+		decoder := json.NewDecoder(resp.Body)
+		err = decoder.Decode(&request)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
 
 func BenchmarkFirst(b *testing.B) {
 	b.ResetTimer()
